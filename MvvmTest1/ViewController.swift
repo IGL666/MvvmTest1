e68a923ef28acdc1036d422527c0690fc3dddbb6
INGWANG LEE <93109525+leegg666@users.noreply.github.com>
2021-12-21 19:27:53 +0900

    Initial Commit

---
+++ MvvmTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MvvmTest1
+//
+//  Created by ggwang on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
